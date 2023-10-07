f8d3c24970e2a374b2448b90b71e9cd936c39164
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-07 22:56:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-# portscanner 
-# dibuat oleh fixploit14
+# Port Scanner
+# Created by fixploit14
 
 import socket
 
-alamat_ip = input("Masukkan alamat IP: ")
-port_range = input("Masukkan rentang port awal dan akhir yang dipisahkan dengan tanda strip (contoh: 80-443): ").split('-')
+text="Port Scanner"
+
+print("-" * 35)
+print(f"{text:^35}")
+print("-" * 35)
+
+ip_address = input("Enter IP address: ")
+port_range = input("Enter the range of ports, separated by a hyphen (e.g., 80-443): ").split('-')
 port_start = int(port_range[0])
 port_end = int(port_range[1])
 
 socket.setdefaulttimeout(5)
 
-print("-"*35)
+print("-" * 35)
 for port in range(port_start, port_end + 1):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((alamat_ip, port))
-        print(f"[+] Port {port} terbuka")
+        s.connect((ip_address, port))
+        print(f"[+] Port {port} is open")
         s.close()
     except (socket.timeout, ConnectionRefusedError):
         pass
-print("-"*35)
+print("-" * 35)
