5a946798130715e34e7c9c6ba409b6fd38f5a2d8
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-07 22:42:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# portscanner
+# dibuat oleh fixploit14
+
 import socket
 
-ipaddress = input("Masukkan alamat ip: ")
-daftar_port= input("Masukkan nomor port yang dipisahkan dengan koma (misalnya: 80,443,22): ").split(',')
-daftar_port= [int(port) for port in ports]
+alamat_ip = input("Masukkan alamat IP: ")
+daftar_port = input("Masukkan daftar port yang dipisahkan dengan koma (contoh: 80,443,22): ").split(',')
+daftar_port = [int(port) for port in daftar_port]
 
 socket.setdefaulttimeout(5)
 
 for port in daftar_port:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
+        s.connect((alamat_ip, port))
         print(f"Port {port} terbuka")
         s.close()
     except (socket.timeout, ConnectionRefusedError):
