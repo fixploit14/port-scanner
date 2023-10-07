1e07c1699ebdbbfe6e7475fb40bf577b7e8287b5
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-08 00:06:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ print(f"{by:^70}")
 print(f"{github:^70}")
 print("-" * 70)
 
-ip_address = input("Enter IP address: ")
-start_port = int(input("Enter start port: "))
-end_port = int(input("Enter end port: "))
+ip_address = input("          Enter IP address: ")
+start_port = int(input("          Enter start port: "))
+end_port = int(input("          Enter end port: "))
 
 socket.setdefaulttimeout(5)
 
-print("-" * 35)
+print("-" * 70)
 for port in range(start_port, end_port + 1):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,4 +28,4 @@ for port in range(start_port, end_port + 1):
         s.close()
     except (socket.timeout, ConnectionRefusedError):
         pass
-print("-" * 35)
+print("-" * 70)
