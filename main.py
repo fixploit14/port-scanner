7bbcc39084fde5eddbdea3e083215462ee197b3c
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-08 10:36:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 # Port Scanner
 # Created by fixploit14
 
-import os
 import socket
-
-program="Port Scanner"
-by="Created by fixploit14"
-github="Github: https://github.com/fixploit14/port-scanner"
-
-os.system("clear")
-
-print("-" * 70)
-print(f"{program:^70}")
-print(f"{by:^70}")
-print(f"{github:^70}")
-print("-" * 70)
 
 ip_address = input("          Enter IP address: ")
 start_port = int(input("          Enter start port: "))
