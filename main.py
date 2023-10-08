c7c4e9f6f87a9266a5306c6593d1186f3f1cc8b3
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-08 10:38:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 
 import socket
 
-ip_address = input("          Enter IP address: ")
-start_port = int(input("          Enter start port: "))
-end_port = int(input("          Enter end port: "))
+ip_address = input("Enter IP address: ")
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
 
 socket.setdefaulttimeout(5)
 
@@ -14,7 +14,7 @@ for port in range(start_port, end_port + 1):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip_address, port))
-        print(f"          [+] Port {port} is open")
+        print(f"[+] Port {port} is open")
         s.close()
     except (socket.timeout, ConnectionRefusedError):
         pass
