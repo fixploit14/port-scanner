8e8329fe230a8a6df6018c473d218208a0e13db4
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-08 10:28:20 +0700

    Update main.py
    menambhakan port is closed

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ for port in range(start_port, end_port + 1):
         print(f"          [+] Port {port} is open")
         s.close()
     except (socket.timeout, ConnectionRefusedError):
-        pass
+        print(f"          [+] Port {port} is closed")
 print("-" * 70)
